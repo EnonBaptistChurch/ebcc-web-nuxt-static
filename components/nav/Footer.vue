8b04d58cf41f2987b37f60ce07a274724d478312
David <davidmitchellfootball@googlemail.com>
2025-10-10 13:48:45 +0100

    rearrange footer

---
+++ components/nav/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="footer">
+    <div class="footer-section">
+      <p class="left-align-section">© Enon Baptist Church 2025</p>
+      <br />
+      <p>
+        Enon Baptist Church, Skinner Street, Chatham, ME4 5RF, UK
+        (<NuxtLink href="/find-us">Find Us</NuxtLink>)
+      </p>
+
+    </div>
+  </div>
+</template>
+<style scoped src="./Footer.css"></style>
