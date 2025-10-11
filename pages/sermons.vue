2bc56715fe85914f47c87f32fbc6e3a63c2dfa85
David <davidmitchellfootball@googlemail.com>
2025-10-11 16:47:59 +0100

    configured a few things with chosen and choosing episode

--- pages/sermons.vue
+++ pages/sermons.vue
@@ -7,34 +7,40 @@
     </SermonWidget>
     
     <div v-if="podcast">
-      <AudioPlayerWidget src='https://d3ctxlq1ktw2nl.cloudfront.net/staging/2025-7-31/f9492f2f-fce2-1315-c70f-db32df91e3c3.mp3'/>
-      <RssFeedSermonComponent :episodes="podcast.items" />
+      <AudioPlayerWidget :src="chosenPodcast"/>
+      <RssFeedSermonsComponent :episodes="podcast.items" :choose-podcast="choosePodcast" :chosen-podcast="chosenPodcast" />
     </div>
     <div v-else>
       <p>Loading sermons...</p>
-    </div>    
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue'
 import SermonWidget from '../components/sermons/SermonWidget.vue'
-import RssFeedSermonComponent from '../components/sermons/RssFeedSermonComponent.vue'
+import RssFeedSermonsComponent from '../components/sermons/RssFeedSermonsComponent.vue'
 import AudioPlayerWidget from '../components/widgets/AudioPlayerWidget.vue'
+import type { PodcastItem } from '@/types/SermonPodcasts';
+
 
 const podcast = ref<any>(null);
 const chosenPodcast = ref<any>(null);
-const fetchedNoPodcastSuccessfully = ref<boolean|null>(null);
+const fetchedNoPodcastSuccessfully = ref<boolean|null>(false);
 
 onMounted(async () => {
   try {
     const data = await fetch("/.netlify/functions/sermons").then(res => res.json())
     podcast.value = data
-    console.log("Podcast data loaded:", data)
+    choosePodcast(data.items[0]);
   } catch (err) {
-    console.error("Failed to load podcast feed:", err)
     podcast.value = [];
     fetchedNoPodcastSuccessfully.value = true;
   }
 })
+
+const choosePodcast = (podcast: PodcastItem) => {
+  chosenPodcast.value = podcast;
+  window.scrollTo({ top: 0, behavior: 'smooth' })
+}
 </script>
