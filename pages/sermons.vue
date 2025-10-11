44f981f9c8da3325d08fe6824c029e3240f1779c
David <davidmitchellfootball@googlemail.com>
2025-10-11 01:41:36 +0100

    change sermons page to incorporate the audio player and so on

--- pages/sermons.vue
+++ pages/sermons.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="sermons-page">
     <h1>Recent Sermons</h1>
-    <p>Here is our latest uploaded sermon.</p>
-    <SermonWidget />
+    
+    <SermonWidget v-if="fetchedNoPodcastSuccessfully">
+      <p>Here is our latest uploaded sermon.</p>
+    </SermonWidget>
+    
+    <div v-if="podcast">
+      <AudioPlayerWidget src='https://d3ctxlq1ktw2nl.cloudfront.net/staging/2025-7-31/f9492f2f-fce2-1315-c70f-db32df91e3c3.mp3'/>
+      <RssFeedSermonComponent :episodes="podcast.items" />
+    </div>
+    <div v-else>
+      <p>Loading sermons...</p>
+    </div>    
   </div>
 </template>
 
-<script>
-// Import the SermonWidget component (ensure it is in the correct directory)
-import SermonWidget from '~/components/widgets/SermonWidget'
+<script setup lang="ts">
+import { ref, onMounted } from 'vue'
+import SermonWidget from '../components/sermons/SermonWidget.vue'
+import RssFeedSermonComponent from '../components/sermons/RssFeedSermonComponent.vue'
+import AudioPlayerWidget from '../components/widgets/AudioPlayerWidget.vue'
 
-export default {
-  components: {
-    SermonWidget
+const podcast = ref<any>(null);
+const chosenPodcast = ref<any>(null);
+const fetchedNoPodcastSuccessfully = ref<boolean|null>(null);
+
+onMounted(async () => {
+  try {
+    const data = await fetch("/.netlify/functions/sermons").then(res => res.json())
+    podcast.value = data
+    console.log("Podcast data loaded:", data)
+  } catch (err) {
+    console.error("Failed to load podcast feed:", err)
+    podcast.value = [];
+    fetchedNoPodcastSuccessfully.value = true;
   }
-}
+})
 </script>
