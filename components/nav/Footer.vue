5aacff67a98d2a700e9286ab3c6616eeec7b97d5
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2025-10-15 22:04:42 +0100

    tidy up nav links

--- components/nav/Footer.vue
+++ components/nav/Footer.vue
@@ -74,52 +74,10 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
+import { computed } from 'vue';
 import FooterColumn from './FooterColumn.vue';
+import { aboutSection, whatsonSection, whatsaChristianSection, sermonSection, findUsSection, contactSection }  from '../../data/NavigationLinks'
 
-const aboutSection = {
-  title: "About Us",
-  link: "/about",
-  submenu: [
-    { link: "/about/what-we-believe/", title: "What We Believe" },
-    { link: "/about/church-history/", title: "Church History" },
-    { link: "/about/pastors-message/", title: "Pastor's Message" },
-  ]
-};
-
-const whatsonSection = {
-  title: "What's on",
-  submenu: [
-    { link: "/whats-on/sunday-services/", title: "Sunday Services" },
-    { link: "/whats-on/bible-study/", title: "Bible Study" },
-    { link: "/whats-on/womens-meetings/", title: "Women's Meetings" },
-    { link: "/whats-on/online-prayer-meeting/", title: "Online Prayer Meeting" },
-    { link: "/whats-on/mens-meetings/", title: "Men's Meetings" },
-    { link: "/whats-on/youth/", title: "Discoverers" },
-    { link: "/whats-on/children/", title: "Children" },
-    { link: "/whats-on/first-steps/", title: "First Steps" },
-  ],
-  link: "/whats-on"
-};
-
-const whatsaChristianSection = {
-  title: "What Is A Christian?",
-  submenu: [
-    { link: "/what-is-a-christian/becoming-a-christian/", title: "Becoming a Christian" },
-    { link: "/what-is-a-christian/stop-trying-and-start-trusting/", title: "Stop Trying and Start Trusting" }
-  ],
-  link: "/what-is-a-christian/becoming-a-christian/"
-};
-
-const testimonySection = {
-  title: "Testimonies",
-  submenu: [{ link: "/testimonies/", title: "Graham Trice" }],
-  link: "/testimonies/"
-};
-
-const sermonSection = { title: "Sermons", link: "/sermons/" };
-const findUsSection = { title: "Find Us", link: "/find-us/" };
-const contactSection = { title: "Contact Us", link: "/contact-us/" };
 
 const currentYear = computed(() => new Date().getFullYear());
 </script>
