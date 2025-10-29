dd9ec704ed207e856d71fd946121c73a37a58f3e
David <davidmitchellfootball@googlemail.com>
2025-10-29 14:55:55 +0000

    add icons

---
+++ components/widgets/audio/icons/PauseIcon.vue
@@ -0,0 +1,16 @@
+<template>
+  <svg
+    :width="size"
+    :height="size"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    fill="currentColor"
+  >
+    <path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/>
+  </svg>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ size?: number }>()
+const size = props.size || 24
+</script>
