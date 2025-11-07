340b3a10b81430a14b5fe0ebaa40514290b0f098
David <davidmitchellfootball@googlemail.com>
2025-11-07 22:50:56 +0000

    added meta description

--- pages/whats-on/sunday-morning.vue
+++ pages/whats-on/sunday-morning.vue
@@ -3,4 +3,13 @@
 </template>
 <script setup lang="ts">
 import SundayMorning from '../../components/sundays/SundayMorning.vue';
+useHead({
+  title: 'Sunday Morning Service - Enon Baptist Church',
+  meta: [
+    {
+      name: 'description',
+      content: 'Sunday Morning Service at Enon Baptist Church, Chatham: Bible teaching, singing hymns, and worshiping God together.'
+    }
+  ]
+})
 </script>
