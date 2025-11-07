67a37db476020172ecf87b298b570cd3d82dabd6
David <davidmitchellfootball@googlemail.com>
2025-11-07 22:49:10 +0000

    added meta description

--- pages/testimonies/index.vue
+++ pages/testimonies/index.vue
@@ -6,3 +6,15 @@ import GrahamTrice from '~/components/testimonies/graham-trice.vue';
 <template>
     <GrahamTrice />
 </template>
+
+<script>
+useHead({
+  title: 'Testimonies - Enon Baptist Church',
+  meta: [
+    {
+      name: 'description',
+      content: 'Read the testimonies of people at Enon Baptist Church, Chatham.'
+    }
+  ]
+})
+</script>
