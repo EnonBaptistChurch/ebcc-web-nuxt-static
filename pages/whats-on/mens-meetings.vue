8c9b68b542d6acd8a54dd8d34be7010a834f88a2
David <davidmitchellfootball@googlemail.com>
2025-11-07 22:50:27 +0000

    added meta description

--- pages/whats-on/mens-meetings.vue
+++ pages/whats-on/mens-meetings.vue
@@ -4,3 +4,15 @@
         <p>On Sunday afternoons after a lunch in the church building there is a Men’s meeting at 2pm. These meetings are announced in the morning service and can be seen on the church’s notice sheet a week before.</p>
     </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: "Men's Meetings - Enon Baptist Church",
+  meta: [
+    {
+      name: 'description',
+      content: "Men's meetings happen after the morning service at Enon Baptist Church, Chatham. A time for fellowship, discussion, and grow among men."
+    }
+]
+});
+</script>
