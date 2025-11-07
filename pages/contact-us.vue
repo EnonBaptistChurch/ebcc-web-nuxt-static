ce81341c3b67be563ab7896937467ca9fded4320
David <davidmitchellfootball@googlemail.com>
2025-11-07 22:47:41 +0000

    added meta description

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -11,4 +11,13 @@ import NetlifyContact from '~/components/NetlifyContact.vue';
 export default {
   name: 'ContactPage'
 }
+useHead({
+  title: 'Contact Us - Enon Baptist Church',
+  meta: [
+    {
+      name: 'description',
+      content: 'Get in touch with Enon Baptist Church, Chatham. Fill out our contact form or find our phone number to reach us for any inquiries or information.'
+    }
+  ]
+})
 </script>
