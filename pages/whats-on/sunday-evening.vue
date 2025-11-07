f163133dab948bdc126c05492a12c458037fd8fc
David <davidmitchellfootball@googlemail.com>
2025-11-07 22:50:45 +0000

    added meta description

--- pages/whats-on/sunday-evening.vue
+++ pages/whats-on/sunday-evening.vue
@@ -3,4 +3,13 @@
 </template>
 <script setup lang="ts">
 import SundayEvening from '../../components/sundays/SundayEvening.vue';
+useHead({
+  title: 'Sunday Evening Service - Enon Baptist Church',
+  meta: [
+    {
+      name: 'description',
+      content: 'Join us for the Sunday Evening Service at Enon Baptist Church, Chatham. We sing, we pray and hear from God\'s word together.'
+    }
+  ]
+})
 </script>
