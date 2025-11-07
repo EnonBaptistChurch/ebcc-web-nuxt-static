4fa3cc08ee165beece848271dfd65b52b339a4d1
David <davidmitchellfootball@googlemail.com>
2025-11-07 23:04:42 +0000

    refactor: update contact page to use script setup syntax

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -5,12 +5,9 @@
 </template>
 
 
-<script>
-import NetlifyContact from '~/components/NetlifyContact.vue';
+<script setup lang="ts">
+import NetlifyContact from '../components/NetlifyContact.vue';
 // import Contact from '~/components/Contact.vue';
-export default {
-  name: 'ContactPage'
-}
 useHead({
   title: 'Contact Us - Enon Baptist Church',
   meta: [
