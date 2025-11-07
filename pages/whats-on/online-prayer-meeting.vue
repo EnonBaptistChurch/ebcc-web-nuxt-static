d792134653f4ac6995729e3158beb161f3fbb3f7
David <davidmitchellfootball@googlemail.com>
2025-11-07 22:50:38 +0000

    added meta description

--- pages/whats-on/online-prayer-meeting.vue
+++ pages/whats-on/online-prayer-meeting.vue
@@ -4,3 +4,14 @@
         <a href= "https://us02web.zoom.us/j/84104984721?pwd=UG5wNGp2eXpDUWtUVmZoRFBPcjVvUT09">Click Here for Zoom Link</a>
     </div>
 </template>
+<script setup lang="ts">
+useHead({
+  title: 'Online Prayer Meeting - Enon Baptist Church',
+  meta: [
+    {
+      name: 'description',
+      content: 'Online prayer meetings at Enon Baptist Church, Chatham are for those who can\'t attend midweek meetings regularly.'
+    }
+  ]
+});
+</script>
