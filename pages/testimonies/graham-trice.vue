70b1826ad9718e8020739d00786fc8a6e76eadd1
David <davidmitchellfootball@googlemail.com>
2025-11-07 22:49:05 +0000

    added meta description

---
+++ pages/testimonies/graham-trice.vue
@@ -0,0 +1,19 @@
+<script setup>
+import GrahamTrice from '~/components/testimonies/graham-trice.vue';
+
+</script>
+
+<template>
+    <GrahamTrice />
+</template>
+<script>
+useHead({
+  title: 'Testimonies - Graham Trice - Enon Baptist Church',
+  meta: [
+    {
+      name: 'description',
+      content: 'From “Man of Kent” to “Man of God”: A Life Transformed by Grace - Read Graham Trice’s testimony at Enon Baptist Church, Chatham.'
+    }
+  ]
+})
+</script>
