c694acaf961831c1c3ee8e7a8a0dc47a1b06b7e6
David <davidmitchellfootball@googlemail.com>
2025-12-07 02:14:37 +0000

    changed parsing system

--- utils/sermonUtils.ts
+++ utils/sermonUtils.ts
@@ -1,20 +1,53 @@
 import type { ParsedSnippet } from "@/types/SermonPodcasts"
 import { getSermonDate } from "@/composables/UseSermonDateConverter";
 export function parseContentSnippet(snippet: string): ParsedSnippet |null {
-  const lines = snippet
-    .split("\n")
-    .map(line => line.trim())
-    .filter(Boolean);
-    if(lines.length < 3) return null;
-    const hasValidDate = parseDateString(lines[2]!) !== 0;
-    if(!hasValidDate) return null;
-  return {
-    title: lines[0] || "",
-    speaker: lines[1] || "",
-    date: lines[2] || "",
-    service: lines[3] || "",
-    formattedDate: getSermonDate(lines[2]!) || undefined
+  // const lines = snippet
+  //   .split("\n")
+  //   .map(line => line.trim())
+  //   .filter(Boolean);
+  //   if(lines.length < 3) return null;
+  //   const hasValidDate = parseDateString(lines[2]!) !== 0;
+  //   if(!hasValidDate) return null;
+    
+  // return {
+  //   title: lines[0] || "",
+  //   speaker: lines[1] || "",
+  //   date: lines[2] || "",
+  //   service: lines[3] || "",
+  //   series: lines[4] || "",
+  //   formattedDate: getSermonDate(lines[2]!) || undefined
+  // 
+  
+  const lines = [...snippet.matchAll(/<p>(.*?)<\/p>/gi)]
+  .map(m => (m[1]?.trim() || "").replace(/&amp;/g, "&"));
+  
+
+  // Ensure we have at least 3 lines (title, speaker, date)
+  if (lines.length < 3) return null;
+
+  const date = lines[2];
+  if (!date || parseDateString(date) === 0) return null;
+
+  if(lines.length>4 && lines.findIndex(l=>l.toLowerCase().includes('reference'))>-1){
+    console.log(snippet, 'invalid snippet');
+    console.log(lines[5], 'invalid snippet');
   }
+
+  const speaker = deDupeGT(lines[1] != null? lines[1] : "")
+  return {
+    title: lines[0] ?? "",
+    speaker: speaker,
+    date,
+    service: lines[3] ?? "",
+    series: lines[4] ?? "",
+    formattedDate: getSermonDate(date) ?? undefined
+  };
+}
+
+function deDupeGT(preacher: string): string {
+  if(!preacher) return preacher;
+  if(preacher.includes("Trice")) return "Graham Trice";
+  return preacher;
 }
 
 /**
