1d21020f54fdd417cd27132977fbc7b3ff495f35
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2026-04-19 00:18:46 +0100

    update for trigger

--- pages/im-new/index.vue
+++ pages/im-new/index.vue
@@ -5,7 +5,6 @@
             <p>Whether it is your first time at Enon or are new to Christianity, you would be welcome to attend our meetings.</p>
             <p>We have many different activities across the week for different people, but if it is your first time, we would encourage you to come Sunday morning.</p>
             <p>On a Sunday morning you will be greeted by some of our Church members, who will be more than happy to help you.</p>
-
             <p>If you would like to learn more about Christianity, please speak to our Pastor.</p>
         </div>
     </div>
