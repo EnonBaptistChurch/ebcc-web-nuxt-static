fb9bfa642286add21a8a7367c02832e21dbab781
David <davidmitchellfootball@googlemail.com>
2026-04-30 01:04:41 +0100

    tidy up formatting

--- components/testimonies/simon-asante.vue
+++ components/testimonies/simon-asante.vue
@@ -34,43 +34,44 @@
         <p>
             My Uncle however, understood the message of the gospel and became a “Born again” Christian.
         </p>
-        <p>At the age of 13, I was fast-tracked into boarding school to start my Secondary level of education.
-This is the practice in Ghana; most secondary schools are boarding schools. This is the stage where
-my Christian way of life - which I barely understood, was put to test. Students from all sorts of
-background were brought together, and for most people in Africa, this is where our future is shaped
-as we grow up as teenagers.
+        <p>
+            At the age of 13, I was fast-tracked into boarding school to start my Secondary level of education.
+            This is the practice in Ghana; most secondary schools are boarding schools. 
+            This is the stage where my Christian way of life - which I barely understood, was put to test. 
+            Students from all sorts of background were brought together, and for most people in Africa, this is where our future is shaped as we grow up as teenagers.
         </p>
-        <p>Very soon, I started learning about other things in life which brought fun to a teenager that I was.
-Peer pressure was at play and I began to learn a few vices, like drinking alcohol and sneaking out
-from school into night clubs at weekends. I broke all the rules in school with my friends and
-managed not to get caught. There were other things that were more fun to me at this moment than
-going to church despite the strong Christian influence on the school campus. Every student was
-given a Gideons New Testament with Psalms and Proverbs. I left mine in my box and never read it.
+        <p>
+            Very soon, I started learning about other things in life which brought fun to a teenager that I was.
+            Peer pressure was at play and I began to learn a few vices, like drinking alcohol and sneaking out from school into night clubs at weekends. 
+            I broke all the rules in school with my friends and managed not to get caught. 
+            There were other things that were more fun to me at this moment than going to church despite the strong Christian influence on the school campus. 
+            Every student was given a Gideons New Testament with Psalms and Proverbs. I left mine in my box and never read it.
         </p>
-        <p>Church was not a thing for me anymore, although I always had it at the back of my mind that I had
-to be right with God someday. Something always reminded me that I wasn&#39;t on the right path and I
-needed to turn to Christ.
+        <p>
+            Church was not a thing for me anymore, although I always had it at the back of my mind that I had to be right with God someday. 
+            Something always reminded me that I wasn't on the right path and I needed to turn to Christ.
         </p>
-        <p>I constantly struggled thinking about how I could be right with God. I thought I had to do it myself,
-and that if I could stop my drinking and changed my life, then Jesus would accept me.
+        <p>
+            I constantly struggled thinking about how I could be right with God. 
+            I thought I had to do it myself, and that if I could stop my drinking and changed my life, then Jesus would accept me.
         </p>
-        <p>This continued all the way through university. God always had His eyes on me. Whilst in University
-back home in Ghana, I escaped death twice. One in a near fatal car accident in which a drank-driver
-nearly crashed into my side of the car I was travelling in with my cousin. The other was a football
-stadium disaster in which about 140 people perished in Ghana in the year 2000.
+        <p>
+            This continued all the way through university. God always had His eyes on me. 
+            Whilst in University back home in Ghana, I escaped death twice. 
+            One in a near fatal car accident in which a drank-driver nearly crashed into my side of the car I was travelling in with my cousin. 
+            The other was a football stadium disaster in which about 140 people perished in Ghana in the year 2000.
         </p>
         <p>
             This causes me to constantly ask myself even till today, "What if I had died at that time"? 
             However,God always had His eyes on me.
         </p>
         <p>
-            My wayward lifestyle continued when I came to the UK. When I met my lovely wife and we had our
-            first two children, nothing would push me to go to church. My wife constantly quarrelled with me as
-            she wanted us to raise our children in the Christian way. Shortly the pressure from my wife began
-            to influence me and I started attending church in London where our first daughter was Christened
-            and dedicated. I became the Church Accountant and the responsibility demanded that I always
-            attended the church. I still didn't know a lot about the Bible as the Church was not deeply focused
-            on Bible teaching.
+            My wayward lifestyle continued when I came to the UK. 
+            When I met my lovely wife and we had our first two children, nothing would push me to go to church. 
+            My wife constantly quarrelled with me as she wanted us to raise our children in the Christian way. 
+            Shortly the pressure from my wife began to influence me and I started attending church in London where our first daughter was Christened and dedicated. 
+            I became the Church Accountant and the responsibility demanded that I always attended the church. 
+            I still didn't know a lot about the Bible as the Church was not deeply focused on Bible teaching.
         </p>
         <p>
             In 2014, during the dedication of our second daughter, a friend of mine came to our Church in London. 
@@ -78,43 +79,45 @@ stadium disaster in which about 140 people perished in Ghana in the year 2000.
             My lack of knowledge of the Bible was laid bare.
         </p>
         <p>
-            This caused me to dig deep to find the truth from the Bible. With the help of my late Christian
-Uncle in London, I began my search to find God's truth.
+            This caused me to dig deep to find the truth from the Bible. 
+            With the help of my late Christian Uncle in London, I began my search to find God's truth.
         </p>
         <p>
-            Strangely, this was the first time the Bible had some meaning to me when I read it. Every word was
-well understood and the words looked new and interesting.
+            Strangely, this was the first time the Bible had some meaning to me when I read it. 
+            Every word was well understood and the words looked new and interesting.
         </p>
         <p>
-            One evening I was listening to a Bible teaching on Paul's Epistle to the Romans. The Preacher got to
-Romans Chapter 3 in the series and started explaining the Righteousness of God through faith.
+            One evening I was listening to a Bible teaching on Paul's Epistle to the Romans. 
+            The Preacher got to Romans Chapter 3 in the series and started explaining the Righteousness of God through faith.
         </p>
         <p>
-            This message pierced through my heart and I leapt for joy about what The Lord Jesus Christ has
-done for me as a sinner. I understood that salvation was not by works but by grace through faith. I
-realised how I couldn't do anything to earn God's righteousness. Christ has already done it. I called
-my wife and explained it to her. It was the happiest day of my life!
+            This message pierced through my heart and I leapt for joy about what The Lord Jesus Christ has done for me as a sinner. 
+            I understood that salvation was not by works but by grace through faith. 
+            I realised how I couldn't do anything to earn God's righteousness. 
+            Christ has already done it. 
+            I called my wife and explained it to her. 
+            It was the happiest day of my life!
         </p>
         <p>
-            After this change in my heart and in my life, I realised I needed a Bible believing church where the
-focus is purely on preaching and teaching the Word of God. The church service in my previous
-church didn’t seem to make sense to me any longer. I was hungry and eager to learn more of the
-Scriptures.
+            After this change in my heart and in my life, I realised I needed a Bible believing church where the focus is purely on preaching and teaching the Word of God. 
+            The church service in my previous church didn’t seem to make sense to me any longer. 
+            I was hungry and eager to learn more of the Scriptures.
         </p>
         <p>
-            I began to look for a Bible focused local church. My search finally landed on Enon Baptist Church. I
-was familiar with Enon as my Children used to attend a program on Thursday mornings. I had, however, never attended the main church services.
+            I began to look for a Bible focused local church. 
+            My search finally landed on Enon Baptist Church. 
+            I was familiar with Enon as my Children used to attend a program on Thursday mornings. 
+            I had, however, never attended the main church services.
         </p>
         <p>
-            My first visit to Enon felt great. The Pastor preached on salvation for the soul and the focus on the
-gospel aligned with my expectations. The words from the hymns that were sung expanded my
-knowledge of the Saviour, the Lord Jesus Christ. I decided to make Enon my home and after several
-weeks of attending, I approached the Pastor to enquire about baptism and church membership.
+            My first visit to Enon felt great. 
+            The Pastor preached on salvation for the soul and the focus on the gospel aligned with my expectations. 
+            The words from the hymns that were sung expanded my knowledge of the Saviour, the Lord Jesus Christ. 
+            I decided to make Enon my home and after several weeks of attending, I approached the Pastor to enquire about baptism and church membership.
         </p>
         <p>
-            The constant thought of how Jesus Christ has justified me freely fills my heart with joy and being a
-            Christian has completely changed my whole perspective of life. I want to thank God for saving me
-            and giving me Hope and a new lease of Life.
+            The constant thought of how Jesus Christ has justified me freely fills my heart with joy and being a Christian has completely changed my whole perspective of life. 
+            I want to thank God for saving me and giving me Hope and a new lease of Life.
         </p>
         <p>
             <strong>
