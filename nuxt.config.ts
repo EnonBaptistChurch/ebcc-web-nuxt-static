abf6305d8274975fe1e332cc1c19bec3b7f316ea
David <davidmitchellfootball@googlemail.com>
2026-05-12 14:45:00 +0100

    fix again?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,8 +18,6 @@ export default defineNuxtConfig({
       ],
       link: [
         { rel: 'icon',type: 'image/x-icon', href: '/images/new-logo.ico' },  
-        { rel: 'preload', href: '/css/site.css', as: 'style' },  
-        { rel: 'stylesheet', href: '/css/site.css' },
       ],
       htmlAttrs: {
         lang: 'en'
