6d87e85a72beb16fd4034762471a574a60aedb1b
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2026-06-25 12:36:30 +0100

    changed bio

--- data/ChurchLeaders.ts
+++ data/ChurchLeaders.ts
@@ -10,8 +10,9 @@ export const Elders: Elder[] = [
     {
         name: "Paul Relf",
         elderTitle: "Pastor",
-        bio: "Paul became our pastor in May 2006.",
-        photo: ""
+        bio: "<p>Paul became our pastor in June 2006, after a period of itinerate preaching. </p> Before entering pastoral ministry, he worked as a partner in a small firm of Chartered Accountants in London where he dealt with several charities In addition to other clients. "+
+        "<p> Married to Bridget, they have four grown children and a number of grandchildren. In his free time, he enjoys gardening, watching the news and playing games. </p><p> As well as serving as our pastor, he also serves on the board of directors of <a href=\"https://gbtc.org.uk/\">Grace Baptist Trust Corporation</a> and occasionally writes book reviews for the <a href=\"https://www.evangelical-times.org/author/paul-relf/\">Evangelical Times</a>.</p>",
+        photo: "/images/leaders/PR-8258.webp"
     },
     {
         name: "Graham Trice",
@@ -22,3 +23,4 @@ export const Elders: Elder[] = [
     }
 ]
 
+ 
