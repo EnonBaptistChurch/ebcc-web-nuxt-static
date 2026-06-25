5d52bfcffa6eaf0e53d8b7f0d55dc09395be1310
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2026-06-25 14:52:54 +0100

    add extra tags

--- data/ChurchLeaders.ts
+++ data/ChurchLeaders.ts
@@ -10,7 +10,7 @@ export const Elders: Elder[] = [
     {
         name: "Paul Relf",
         elderTitle: "Pastor",
-        bio: "<p>Paul became our pastor in June 2006, after a period of itinerate preaching. </p> Before entering pastoral ministry, he worked as a partner in a small firm of Chartered Accountants in London where he dealt with several charities In addition to other clients. "+
+        bio: "<p>Paul became our pastor in June 2006, after a period of itinerate preaching. </p> <p>Before entering pastoral ministry, he worked as a partner in a small firm of Chartered Accountants in London where he dealt with several charities In addition to other clients.</p> "+
         "<p> Married to Bridget, they have four grown children and a number of grandchildren. In his free time, he enjoys gardening, watching the news and playing games. </p><p> As well as serving as our pastor, he also serves on the board of directors of <a href=\"https://gbtc.org.uk/\">Grace Baptist Trust Corporation</a> and occasionally writes book reviews for the <a href=\"https://www.evangelical-times.org/author/paul-relf/\">Evangelical Times</a>.</p>",
         photo: "/images/leaders/PR-8258.webp"
     },
