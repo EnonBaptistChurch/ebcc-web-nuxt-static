59609c87b974ce1be8511325dbfebad441c18dc8
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2026-06-25 13:04:03 +0100

    add init testimony data

---
+++ data/Testimonies.ts
@@ -0,0 +1,33 @@
+export type TestimonySnippet ={
+    name: string,
+    shortBio: string,
+    img: string,
+    link:string
+}
+export const testimonies : TestimonySnippet[] = [
+    {
+        name: "Graham Trice",
+        shortBio: "From “Man of Kent” to “Man of God”: A Life Transformed by Grace",
+        img: "/images/GMT-MST-640.webp",
+        link: "/testimonies/graham-trice"
+    },
+    {
+        name: "Georgina Newton",
+        shortBio: "",
+        img: "/images/GN-480.webp",
+        link: "/testimonies/georgina-newton"
+    },
+    {
+        name: "Patrick Nkala",
+        shortBio: "",
+        img: "/images/PN-480.webp",
+        link: "/testimonies/patrick-nkala"
+    },
+    {
+        name: "Simon Asante",
+        shortBio: "",
+        img: "/images/SA-480.webp",
+        link: "/testimonies/simon-asante"
+    },
+
+]
