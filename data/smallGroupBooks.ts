4cfd7f05a7f41acf72f3f392c152c3541ea2df01
David <davidmitchellfootball@googlemail.com>
2026-08-12 14:21:38 +0100

    add former small groups books

---
+++ data/smallGroupBooks.ts
@@ -0,0 +1,15 @@
+import type { Book } from "@/types/book";
+
+export const smallGroupBooks: Book[] = [
+    {
+        title: "Behold I Approach",
+        author: "Tony Payne",
+        imgSrc: "/images/resources/StudyGuides/bold_i_approach_prayer-320.webp"
+    },
+    {
+        title: "Mission",
+        subTitle: "Your place in God's plan for the nations",
+        author: "Alan Purser",
+        imgSrc: "/images/resources/StudyGuides/mission-320.webp"
+    },
+]
