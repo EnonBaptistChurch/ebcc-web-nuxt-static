8dc5d70279258649f02572d9aea533c46f016791
David <davidmitchellfootball@googlemail.com>
2026-08-12 14:21:28 +0100

    add mens meeting books

---
+++ data/mensMeetingBooks.ts
@@ -0,0 +1,10 @@
+import type { Book } from "@/types/book";
+
+export const mensMeetingBooks: Book[] = [
+    {
+        title: "Love Your Church",
+        subTitle: "8 Great Things About Being a Church Member",
+        author: "Tony Merida",
+        imgSrc: "/images/resources/books/love-your-church-320.webp"
+    }
+]
