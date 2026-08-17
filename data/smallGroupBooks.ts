baacbd1148bc5d0150aeb7cb67fbd7dafd2705a2
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2026-08-17 11:43:18 +0100

    Add "Angels" book to small group resources and include corresponding image

--- data/smallGroupBooks.ts
+++ data/smallGroupBooks.ts
@@ -12,4 +12,10 @@ export const smallGroupBooks: Book[] = [
         author: "Alan Purser",
         imgSrc: "/images/resources/StudyGuides/mission-320.webp"
     },
+    {
+        title: "Angels",
+        subTitle: "A Study on God's Special Agents",
+        author: "Graham Trice",
+        imgSrc: "/images/resources/StudyGuides/angels_trice-320.webp"
+    }
 ]
