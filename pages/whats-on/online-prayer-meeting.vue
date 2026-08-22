dc591b2568aa38d783fa05c4621659431b8665e6
davidmitchellfootball <davidmitchellfootball@googlemail.com>
2026-08-22 10:22:17 +0100

    change frequency of  online prayer meeting

--- pages/whats-on/online-prayer-meeting.vue
+++ pages/whats-on/online-prayer-meeting.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="text-content">
-        <p>For those who can not normally get to the weekly prayer meeting, we have an online prayer meeting, usually around once a month.</p>
+        <p>For those who can not normally get to the weekly prayer meeting, we have an online prayer meeting, usually around once a quarter.</p>
         <a href= "https://us02web.zoom.us/j/84104984721?pwd=UG5wNGp2eXpDUWtUVmZoRFBPcjVvUT09">Click Here for Zoom Link</a>
     </div>
 </template>
